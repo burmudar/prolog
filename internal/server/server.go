af2e9a3f11e7b497b5fe8d70679e4956cb34d544
William Bezuidenhout <william.bezuidenhout+github@gmail.com>
2022-08-28 18:55:30 +0200

    impl grpc server

--- internal/server/server.go
+++ internal/server/server.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	api "github.com/burmudar/prolog/api/v1"
+	"google.golang.org/grpc"
 )
 
 type CommitLog interface {
@@ -20,6 +21,17 @@ var _ api.LogServer = (*grpcServer)(nil)
 type grpcServer struct {
 	api.UnimplementedLogServer
 	*Config
+}
+
+func NewGRPCServer(config *Config) (*grpc.Server, error) {
+	gsrv := grpc.NewServer()
+	srv, err := newgrpcServer(config)
+	if err != nil {
+		return nil, err
+	}
+
+	api.RegisterLogServer(gsrv, srv)
+	return gsrv, nil
 }
 
 func newgrpcServer(config *Config) (srv *grpcServer, err error) {
